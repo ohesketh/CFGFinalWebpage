e3dc0573ea02b18157aad6574080fffbc6482d39
bananasaurusrex <bananasaurusrex.rawr@gmail.com>
2016-12-13 14:04:34 +0000

    slideshow working

--- mail_handler.php
+++ mail_handler.php
@@ -1,6 +1,9 @@
-<!doctype php>
+<!doctype html>
+<body>
 <?php 
-if(isset($_POST['submit'])){
+$first_name = $last_name = $message = $email = " ";
+
+if(isset($_POST["submit"])){
     $to = "sew.soc@durham.ac.uk"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
@@ -15,5 +18,12 @@ if(isset($_POST['submit'])){
     mail($to,$subject,$message,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     
+    function test_input($data){
+        $data = trim($data);
+        $data = stripcslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
     }
 ?>
+</body>
